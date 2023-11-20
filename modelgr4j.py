2a629328bceb506ad2c6bac2b1f5efe3a5b93689
Wybren de Jong <wyrdejong@gmail.com>
2023-11-20 15:34:05 +0100

    model compleet, nog geen tijdelement

--- modelgr4j.py
+++ modelgr4j.py
@@ -5,12 +5,15 @@
 import numpy as np
 import pandas as pd
 
+t = 0
+dt = 1
+
 P = 0
 E = 0
 P_n = 0.00
 E_n = 0.00
 S = 0
-
+R = 0 
 x_1 = 0
 x_2 = 0
 x_3 = 0
@@ -44,8 +47,7 @@ S = S - Perc
 P_r = Perc + (P_n - P_s)
 
 # Equation 9 - 15:
-
-
+# SH1
 if t <= 0:
     SH1 = 0
 elif t > 0 and t < x_4:
@@ -53,6 +55,7 @@ elif t > 0 and t < x_4:
 else:
     SH1 = 1
 
+# SH2
 if t <= 0:
     SH2 = 0
 elif t > 0 and t <= x_4:
@@ -62,8 +65,29 @@ elif t > x_4 and t <= 2*x_4:
 else:
     SH2 = 1
 
+# Unit hydrograph determination
 UH1(j) = SH1(j) - SH1(j-1)
 UH2(j) = SH2(j) - SH2(j-1)
+
+Q_1 = 0.1 * P_r * UH1(j)
+Q_9 = 0.9 * P_r * UH2(j)
+
+F = x_2*(R/x_3)**(7/2)
+
+R = max((R + Q_9 + F),0)
+
+Q_r = R*(1-(1+(R/x_3)**4)**(-1/4))
+if Q_r < R:
+    R = R - Q_r
+else:
+    print("Hehe dit gaat fout")
+    exit
+
+Q_d = max((Q_1 + F), 0)
+
+Q = Q_r + Q_d
+
+
 
 
 
