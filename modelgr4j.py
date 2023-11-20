b92bd84af2d0339bcb49a091d839dc780b2c5c59
Wybren de Jong <wyrdejong@gmail.com>
2023-11-20 19:50:59 +0100

    Implementantie UH functies

--- modelgr4j.py
+++ modelgr4j.py
@@ -12,14 +12,45 @@ P = 0
 E = 0
 P_n = 0.00
 E_n = 0.00
-x_1 = 0
-x_2 = 0
-x_3 = 0
-x_4 = 0
+x_1 = 3.5
+x_2 = 4.0
+x_3 = 5.0
+x_4 = 5
 
 S = 0
 R = 0 
 
+def UH1(x_4):
+    SH1 = []
+    UH1 = [0.0]
+    dt1 = 1
+    for t in np.arange(0.0, (x_4+2*dt1), dt1):
+        if t <= 0:
+            SH1.append([t,0])
+        elif t > 0 and t < x_4:
+            SH1.append([t, (t/x_4)**(5/2)])  
+        else:
+            SH1.append([t, 1])
+    for j in range(1,len(SH1),1):
+       UH1.append(SH1[j][1] - SH1[(j-1)][1]) 
+    return UH1
+
+def UH2(x_4):
+    SH2 = []
+    UH2 = [0.0]
+    dt1 = 1
+    for t in np.arange(0,(2*x_4+2*dt1), dt1):
+        if t <= 0:
+            SH2.append([t,0]) 
+        elif t > 0 and t <= x_4:
+            SH2.append([t, 0.5*(t/x_4)**(5/2)])
+        elif t > x_4 and t <= 2*x_4:
+            SH2.append([t, 1-(0.5*(2-(t/x_4))**(5/2))]) 
+        else:
+            SH2.append([t,1])
+    for j in range(1,len(SH2),1):
+       UH2.append(SH2[j][1] - SH2[(j-1)][1]) 
+    return UH2
 
 if P >= E:
     P_n = P - E
@@ -38,38 +69,13 @@ E_s = (S*(2-(S/x_1))*np.tanh(E_n/x_1))/(1+(1-(S/x_1))*np.tanh(E_n/x_1))
 S = S - E_s + P_s
 
 # Equation 6: perculation equation:
-Perc = S(1-(1+((4/9)*(S/x_1)**4))**(-0.25))
+Perc = S*(1-(1+((4/9)*(S/x_1)**4))**(-0.25))
 
 # Equation 7: change in resevoir content
 S = S - Perc
 
 # Equation 8: routing equation
 P_r = Perc + (P_n - P_s)
-
-def UH1(x_4):
-    SH1 = []
-    SH2 = []
-    UH1 = [0]
-    UH2 = [0]
-    dt1 = 0.5
-
-    for t in range(0,(x_4+2*dt1), dt1):
-        if t <= 0:
-            SH1.append([t,0])
-        elif t > 0 and t < x_4:
-            SH1.append([t, (t/x_4)**(5/2)])  
-        else:
-            SH1.append([t, 1])
-    
-    for t in range(0,(2*x_4+2*dt1), dt1):
-        if t <= 0:
-            SH2.append([t,0]) 
-        elif t > 0 and t <= x_4:
-            SH2.append([t, 0.5*(t/x_4)**(5/2)])
-        elif t > x_4 and t <= 2*x_4:
-            SH2.append([t, 1-(0.5*(2-(t/x_4))**(5/2))]) 
-        else:
-            SH2.append([t,1])
 
 # for the range van sh1 en sh2 UH1 en UH2 bepalen
 
@@ -92,27 +98,27 @@ elif t > x_4 and t <= 2*x_4:
 else:
     SH2 = 1
 
-# Unit hydrograph determination
-UH1(j) = SH1(j) - SH1(j-1)
-UH2(j) = SH2(j) - SH2(j-1)
+# # Unit hydrograph determination
 
-Q_1 = 0.1 * P_r * UH1(j)
-Q_9 = 0.9 * P_r * UH2(j)
+# UH2(j) = SH2(j) - SH2(j-1)
 
-F = x_2*(R/x_3)**(7/2)
+# Q_1 = 0.1 * P_r * UH1(j)
+# Q_9 = 0.9 * P_r * UH2(j)
 
-R = max((R + Q_9 + F),0)
+# F = x_2*(R/x_3)**(7/2)
 
-Q_r = R*(1-(1+(R/x_3)**4)**(-1/4))
-if Q_r < R:
-    R = R - Q_r
-else:
-    print("Hehe dit gaat fout")
-    exit
+# R = max((R + Q_9 + F),0)
 
-Q_d = max((Q_1 + F), 0)
+# Q_r = R*(1-(1+(R/x_3)**4)**(-1/4))
+# if Q_r < R:
+#     R = R - Q_r
+# else:
+#     print("Hehe dit gaat fout")
+#     exit
 
-Q = Q_r + Q_d
+# Q_d = max((Q_1 + F), 0)
+
+# Q = Q_r + Q_d
 
 
 
