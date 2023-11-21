3b090abc4403752b11dfc209dd1f9fef3d3b1e77
Wybren de Jong <wyrdejong@gmail.com>
2023-11-21 10:01:35 +0100

    Final merge timestep

--- modelgr4j.py
+++ modelgr4j.py
@@ -5,6 +5,21 @@
 import numpy as np
 import pandas as pd
 
+t = 0
+dt = 1
+
+P = 2.8
+E = 0.4
+P_n = 0.00
+E_n = 0.00
+x_1 = 0.7
+x_2 = 4.0
+x_3 = 5.0
+x_4 = 5
+S = 0
+R = 0 
+
+
 precipitation = pd.read_excel("C:\\Users\\pc-ri\\Documents\\Uni\\18 - Q6 TEM\\Hydrological Modelling and Forecasting\\Assignments\\Hydrological-Modelling-and-Forecasting\\Observed time series 1968-1982.xlsx", 0)
 evapotranspiration = pd.read_excel("C:\\Users\\pc-ri\\Documents\\Uni\\18 - Q6 TEM\\Hydrological Modelling and Forecasting\\Assignments\\Hydrological-Modelling-and-Forecasting\\Observed time series 1968-1982.xlsx", 1)
 discharge = pd.read_excel("C:\\Users\\pc-ri\\Documents\\Uni\\18 - Q6 TEM\\Hydrological Modelling and Forecasting\\Assignments\\Hydrological-Modelling-and-Forecasting\\Observed time series 1968-1982.xlsx", 2)
@@ -19,20 +34,24 @@ def update_timestep(t, P, E, P_n, E_n, S, x_1, x_2, x_3, x_4):
     else:
         P_n = 0.00
         E_n = E - P
-t = 0
-dt = 1
+    
+    # Equation 3: production store
+    P_s = (x_1*(1-(S/x_1)**2)*np.tanh(P_n/x_1))/(1+(S/x_1)*np.tanh(P_n/x_1))
 
-P = 2.8
-E = 0.4
-P_n = 0.00
-E_n = 0.00
-x_1 = 0.7
-x_2 = 4.0
-x_3 = 5.0
-x_4 = 5
+    # Equation 4: storage evaporation
+    E_s = (S*(2-(S/x_1))*np.tanh(E_n/x_1))/(1+(1-(S/x_1))*np.tanh(E_n/x_1))
 
-S = 0
-R = 0 
+    # Equation 5: water content in the production store
+    S = S - E_s + P_s
+
+    # Equation 6: perculation equation:
+    Perc = S*(1-(1+((4/9)*(S/x_1)**4))**(-0.25))
+
+    # Equation 7: change in resevoir content
+    S = S - Perc
+
+    # Equation 8: routing equation
+    P_r = Perc + (P_n - P_s)
 
 def fun_UH1(x_4):
     SH1 = []
@@ -69,65 +88,6 @@ def fun_UH2(x_4):
 UH1 = fun_UH1(x_4)
 UH2 = fun_UH2(x_4)
 
-
-
-if P >= E:
-    P_n = P - E
-    E_n = 0.00
-else:
-    P_n = 0.00
-    E_n = E - P
-
-    # Equation 3: production store
-    P_s = (x_1*(1-(S/x_1)**2)*np.tanh(P_n/x_1))/(1+(S/x_1)*np.tanh(P_n/x_1))
-
-    # Equation 4: storage evaporation
-    E_s = (S*(2-(S/x_1))*np.tanh(E_n/x_1))/(1+(1-(S/x_1))*np.tanh(E_n/x_1))
-
-    # Equation 5: water content in the production store
-    S = S - E_s + P_s
-
-    # Equation 6: perculation equation:
-    Perc = S*(1-(1+((4/9)*(S/x_1)**4))**(-0.25))
-
-    # Equation 7: change in resevoir content
-    S = S - Perc
-
-    # Equation 8: routing equation
-    P_r = Perc + (P_n - P_s)
-
-# Equation 9 - 15:
-
-
-if t <= 0:
-    SH1 = 0
-elif t > 0 and t < x_4:
-    SH1 = (t/x_4)**(5/2)
-else:
-    SH1 = 1
-
-if t <= 0:
-    SH2 = 0
-elif t > 0 and t <= x_4:
-    SH2 = 0.5*(t/x_4)**(5/2)
-elif t > x_4 and t <= 2*x_4:
-    SH2 = 1-(0.5*(2-(t/x_4))**(5/2))
-else:
-    SH2 = 1
-
-UH1(j) = SH1(j) - SH1(j-1)
-UH2(j) = SH2(j) - SH2(j-1)
-
-P = 0
-E = 0
-P_n = 0.00
-E_n = 0.00
-S = 0
-
-x_1 = 350
-x_2 = 0
-x_3 = 90
-x_4 = 1.7
 
 for t in range(len(precipitation)-2):
     P = precipitation.iat[t+2, 5]
