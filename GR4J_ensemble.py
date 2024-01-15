871ebcf2b1e29a53af0b4f5faac773439bf21bc2
Wybren de Jong <wyrdejong@gmail.com>
2024-01-15 14:43:42 +0100

    Modifications to base model

--- GR4J_ensemble.py
+++ GR4J_ensemble.py
@@ -6,8 +6,6 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-import random as rd
-import scipy.stats as ss
 
 #
 # Functions
@@ -100,96 +98,34 @@ def fun_UH2(x_4):
        UH2.append(SH2[j][1] - SH2[(j-1)][1]) 
     return UH2
 
-def KGE(Qmodel, Qreal, warmup):
-    r = ss.pearsonr(Qmodel[warmup:], Qreal[warmup:])
-    a = np.std(Qmodel[warmup:]) / np.std(Qreal[warmup:])
-    b = np.mean(Qmodel[warmup:]) / np.mean(Qreal[warmup:])
-
-    KGE_value = 1 - np.sqrt((r[0]-1)**2 + (a-1)**2 + (b-1)**2)
-
-    return KGE_value
-
-def NSE(Qmodel, Qreal, warmup):
-    part1 = 0
-    part2 = 0
-    for i in range(len(Qmodel)):
-        if i > warmup:
-            part1 += (Qmodel[i] - Qreal[i])**2
-            part2 += (Qreal[i] - np.mean(Qreal))**2
-    
-    NSE_value = 1 - part1 / part2
-
-    return NSE_value
-
-def RVE(Qmodel, Qreal, warmup):
-
-    RVE_value = ((sum(Qmodel[warmup:]) - sum(Qreal[warmup:])) / sum(Qreal[warmup:])) * 100
-
-    return RVE_value
-
 #
 # Init
 #
-
-sens_analysis = False
-calibration = False
 warmup = 365
-verif_length = 500
-fout = False
-validation = True
-
 t = 0
 dt = 1
-x_1 = 350
-sensx1 = [100, 183, 266, 350, 633, 916, 1200]
-x_2 = 0
-sensx2 = [-5, -3.3, -1.6, 0, 1, 2, 3]
-x_3 = 90
-sensx3 = [20, 43, 67, 90, 160, 230, 300]
-x_4 = 1.7
-sensx4 = [1.1, 1.3, 1.5, 1.7, 2.1, 2.5, 2.9]
+x_1 = 166.1
+x_2 = -1.57
+x_3 = 40.3
+x_4 = 2.26
 S = 0
 R = 0
-
 area = 1311
 
 path1 = os.path.join(os.path.dirname(__file__), "Observed time series 1968-1982.xlsx")
 precipitation = pd.read_excel(path1, 0, header=2)
 precip_lesse = precipitation["Lesse"]
-
-if validation:
-    path2 = os.path.join(os.path.dirname(__file__), "Observed time series 1984-1998.xlsx")
-    precipitation2 = pd.read_excel(path2, 0, header=2)
-    precip_lesse2 = precipitation2["Lesse"]
-
-# Constant precipitation
-#precip_lesse = np.zeros(verif_length)
-#for i in range(100):
-#    precip_lesse[i] = 8
 precip_total_9 = np.zeros(len(precip_lesse)+12)
 precip_total_1 = np.zeros(len(precip_lesse)+12)
 
 evapotranspiration = pd.read_excel(path1, 1, header=2)
 evap_lesse = evapotranspiration["Lesse"]
-if validation:
-    evapotranspiration2 = pd.read_excel(path2, 1, header=2)
-    evap_lesse2 = evapotranspiration2["Lesse"]
+
 #evap_lesse = np.zeros(verif_length)
 
 discharge = pd.read_excel(path1, 2, header=2)
 discharge_lesse = discharge["Lesse"]
-if validation:
-    discharge2 = pd.read_excel(path2, 2, header=2)
-    discharge_lesse2 = discharge2["Lesse"]
-
-sens_length = len(precip_lesse)
 total_discharge = np.zeros(len(precip_lesse))
-total_discharge_val = np.zeros(len(precip_lesse2))
-total_discharge1 = np.zeros([7, sens_length])
-total_discharge2 = np.zeros([7, sens_length])
-total_discharge3 = np.zeros([7, sens_length])
-total_discharge4 = np.zeros([7, sens_length])
-KGE_values = np.zeros([4, 7])
 
 #
 # Calculate Unit Hydrographs
@@ -201,201 +137,20 @@ Q_9 = np.zeros([len(precip_lesse), len(precip_lesse)+12])
 #
 # Main
 #
+ 
+UH1 = fun_UH1(x_4)
+UH2 = fun_UH2(x_4)
+for t in range(len(precip_lesse)):
+    P = precip_lesse.iat[t]
+    E = evap_lesse.iat[t]
+    [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
+    total_discharge[t] = Q / 86400 * area * 1000
 
-if sens_analysis:
-    for i in range(len(sensx1)):
-        x_1 = sensx1[i]
-        x_2 = 0
-        x_3 = 90
-        x_4 = 1.7
-        UH1 = fun_UH1(x_4)
-        UH2 = fun_UH2(x_4)
-        for t in range(sens_length):
-            P = precip_lesse.iat[t]
-            E = evap_lesse.iat[t]
-            [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-            total_discharge1[i, t] = Q / 86400 * area * 1000
-        KGE_values[0, i] = KGE(total_discharge1[i,:], discharge_lesse, warmup)
-            
-    for i in range(len(sensx2)):
-        x_1 = 350
-        x_2 = sensx2[i]
-        x_3 = 90
-        x_4 = 1.7
-        UH1 = fun_UH1(x_4)
-        UH2 = fun_UH2(x_4)
-        for t in range(sens_length):
-            P = precip_lesse.iat[t]
-            E = evap_lesse.iat[t]
-            [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-            total_discharge2[i, t] = Q / 86400 * area * 1000
-        KGE_values[1, i] = KGE(total_discharge2[i,:], discharge_lesse, warmup)
-
-    for i in range(len(sensx3)):
-        x_1 = 350
-        x_2 = 0
-        x_3 = sensx3[i]
-        x_4 = 1.7
-        UH1 = fun_UH1(x_4)
-        UH2 = fun_UH2(x_4)
-        for t in range(sens_length):
-            P = precip_lesse.iat[t]
-            E = evap_lesse.iat[t]
-            [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-            total_discharge3[i, t] = Q / 86400 * area * 1000
-        KGE_values[2, i] = KGE(total_discharge3[i,:], discharge_lesse, warmup)
-
-    for i in range(len(sensx4)):
-        x_1 = 350
-        x_2 = 0
-        x_3 = 90
-        x_4 = sensx4[i]
-        UH1 = fun_UH1(x_4)
-        UH2 = fun_UH2(x_4)
-        for t in range(sens_length):
-            P = precip_lesse.iat[t]
-            E = evap_lesse.iat[t]
-            [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-            total_discharge4[i, t] = Q / 86400 * area * 1000
-        KGE_values[3, i] = KGE(total_discharge4[i,:], discharge_lesse, warmup)
-
-elif calibration:
-    bestKGE = 0.7
-    bestx1 = 248
-    bestx2 = -1.21
-    bestx3 = 71
-    bestx4 = 2.19
-    rd.seed(0)
-
-    for k in range(100):
-        randpar = rd.randint(1,4)
-        if randpar == 1:
-            x_1 = rd.normalvariate(bestx1, 50)
-            while x_1 < 100:
-                x_1 = rd.normalvariate(bestx1, 50)
-        elif randpar == 2:
-            x_2 = rd.normalvariate(bestx2, 0.5)
-        elif randpar == 3:
-            x_3 = rd.normalvariate(bestx3, 10)
-        else:
-            x_4 = rd.normalvariate(bestx4, 0.5)
-        second = rd.randint(0,1)
-        if second == 1:
-            randpar2 = rd.randint(1,4)
-            while randpar2 == randpar:
-                randpar2 = rd.randint(1,4)
-            if randpar2 == 1:
-                x_1 = rd.normalvariate(bestx1, 50)
-                while x_1 < 100:
-                    x_1 = rd.normalvariate(bestx1, 50)
-            elif randpar2 == 2:
-                x_2 = rd.normalvariate(bestx2, 0.5)
-            elif randpar2 == 3:
-                x_3 = rd.normalvariate(bestx3, 10)
-            else:
-                x_4 = rd.normalvariate(bestx4, 0.5)
-        
-        UH1 = fun_UH1(x_4)
-        UH2 = fun_UH2(x_4)
-        for t in range(len(precip_lesse)):
-            if not fout:
-                P = precip_lesse.iat[t]
-                E = evap_lesse.iat[t]
-                [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-                total_discharge[t] = Q / 86400 * area * 1000
-            else:
-                break
-        if not fout:
-            KGE_value = KGE(total_discharge, discharge_lesse, warmup)
-            RVE_value = RVE(total_discharge, discharge_lesse, warmup)
-            if KGE_value > bestKGE and abs(RVE_value) < 5:
-                bestKGE = KGE_value
-                bestRVE = RVE_value
-                bestx1 = x_1
-                bestx2 = x_2
-                bestx3 = x_3
-                bestx4 = x_4
-                bestdischarge = total_discharge
-        else:
-            fout = False
-    NSE_value = NSE(total_discharge, discharge_lesse, warmup)
-    print(bestKGE)
-    print(bestRVE)
-    print(NSE_value)
-    print(bestx1)
-    print(bestx2)
-    print(bestx3)
-    print(bestx4)
-    np.savetxt("Calibrated discharge.txt", bestdischarge)
-
-elif validation:
-    x_1 = 166.1
-    x_2 = -1.57
-    x_3 = 40.3
-    x_4 = 2.26
-    UH1 = fun_UH1(x_4)
-    UH2 = fun_UH2(x_4)
-    for t in range(len(precip_lesse2)):
-        P = precip_lesse2.iat[t]
-        E = evap_lesse2.iat[t]
-        [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-        total_discharge_val[t] = Q / 86400 * area * 1000
-    KGE_value = KGE(total_discharge_val, discharge_lesse2, warmup)
-    RVE_value = RVE(total_discharge_val, discharge_lesse2, warmup)
-    NSE_value = NSE(total_discharge_val, discharge_lesse2, warmup)
-    print(KGE_value)
-    print(RVE_value)
-    print(NSE_value)
-
-else:
-    UH1 = fun_UH1(x_4)
-    UH2 = fun_UH2(x_4)
-    for t in range(len(precip_lesse)):
-        P = precip_lesse.iat[t]
-        E = evap_lesse.iat[t]
-        [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
-        total_discharge[t] = Q / 86400 * area * 1000
-    KGE_value = KGE(total_discharge, discharge_lesse, warmup)
-    RVE_value = RVE(total_discharge, discharge_lesse, warmup)
-    NSE_value = NSE(total_discharge, discharge_lesse, warmup)
-    print(KGE_value)
-    print(RVE_value)
-    print(NSE_value)
-    #print(sum(precip_lesse))
-    #print(sum(total_discharge) + R + S)
-
-if sens_analysis:
-    np.savetxt("KGE_Values.txt", KGE_values)
-    for i in range(4):
-        plt.plot(KGE_values[i,:])
-    plt.ylabel("KGE Value")
-    plt.xlabel("Parameter value")
-    plt.legend(["X1", "X2", "X3", "X4"])
-    plt.title("Sensitivity of model to KGE values")
-    #for j in range(len(sensx3)):
-        #plt.plot(total_discharge3[j,:])
-        #plt.ylabel("Discharge (m^3/s)")
-        #plt.xlabel("Time (Days)")
-elif calibration:
-    plt.plot(discharge_lesse)
-    plt.plot(bestdischarge)
-    plt.ylabel("Discharge (m^3/s)")
-    plt.xlabel("Time (Days)")
-    plt.legend(["Measured", "Simulated"])
-elif validation:
-    plt.plot(discharge_lesse2[warmup:])
-    plt.plot(total_discharge_val)
-    plt.ylabel("Discharge (m^3/s)")
-    plt.xlabel("Time (Days)")
-    plt.legend(["Measured", "Simulated"])
-    plt.title("Measured versus simulated discharge")
-    plt.xlim([warmup, len(discharge_lesse2)])
-else:
-    plt.plot(discharge_lesse[warmup:])
-    plt.plot(total_discharge[warmup:])
-    plt.ylabel("Discharge (m^3/s)")
-    plt.xlabel("Time (Days)")
-    plt.legend(["Measured", "Simulated"])
-    plt.title("Measured versus simulated discharge")
-    plt.xlim([warmup, len(discharge_lesse)])
+plt.plot(discharge_lesse[warmup:])
+plt.plot(total_discharge)
+plt.ylabel("Discharge (m^3/s)")
+plt.xlabel("Time (Days)")
+plt.legend(["Measured", "Simulated"])
+plt.title("Measured versus simulated discharge")
+plt.xlim([warmup, len(discharge_lesse)])
 plt.show()
