139ef1c9e3e5cd688bf5327d58b2b2345cdb4c88
Wybren de Jong <wyrdejong@gmail.com>
2024-01-17 11:16:25 +0100

    Merging script

--- GR4J_ensemble.py
+++ GR4J_ensemble.py
@@ -1,7 +1,3 @@
-#
-# https://doi.org/10.1016/S0022-1694(03)00225-7
-#
-
 import numpy as np
 import pandas as pd
 import os
@@ -13,9 +9,9 @@ import matplotlib.pyplot as plt
 
 def dataimport(path, interval):                        
     # Read out the CSV, define index and datacolumn
-    df = pd.read_csv(path, header=0, index_col=None, skiprows=2, delimiter=",", parse_dates=[0], skipinitialspace=True)          
+    df = pd.read_csv(path, header=0, index_col=None, skiprows=2, delimiter=",", parse_dates=[0], skipinitialspace=True, dayfirst=True)          
     # Convert indexcolumn to datetime format                                                                                
-    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y %H:%M',errors='coerce', utc=True)      
+    df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y %H:%M',errors='coerce', utc=True, dayfirst=True)      
     # Drop NAN's                                   
     #df.dropna(subset=['date'], inplace=True) 
     # Convert DataFrame to floats (otherwise errors occur)                                                                                 
@@ -122,13 +118,16 @@ dt = 1
 x_1 = 166.1
 x_2 = -1.57
 x_3 = 40.3
-x_4 = 2.26
+x_4 = 1.5
 S = 0
 R = 0
 area = 1311
-start_date = '2020-12-13'                                     
-end_date = '2021-7-10'                                    
+start_date = '2020-12-13'   
+#start_date = '13-12-2020'                                  
+end_date = '2021-07-10'  
+#end_date = '10-07-2021'                                  
 
+plotprocess = False
 
 df_7_9  = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021070900.csv"), 'D')
 df_7_10 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071000.csv"), 'D')
@@ -136,22 +135,31 @@ df_7_11 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/20210711
 df_7_12 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071200.csv"), 'D')
 df_7_13 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071300.csv"), 'D')
 df_7_14 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071400.csv"), 'D')
-deterministic = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 2, header=0)
-observed_P = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 3, header=0)
-observed_Q = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 5, header=0)
-observed_P['date-time'] = pd.to_datetime(observed_P['date-time'], format='%Y-%m-%d %H:%M:%s', errors='coerce', utc=True)  
-#observed_P = observed_P.loc[start_date:end_date]
-print(observed_P)
-evapotranspiration = np.zeros(len(observed_P)+forecast)
-total_discharge = np.zeros(len(observed_P)+forecast)
+deterministic = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 2, header=0, index_col=None, parse_dates=[0])
+observed_P = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx", ), 4, header=0, index_col=None) 
 
-precip_total_1 = np.zeros(len(observed_P)+12+forecast)
+observed_P['Date'] = pd.to_datetime(observed_P['Date'], format='%Y-%m-%d', errors='coerce', utc=True) 
+observed_P.set_index('Date', inplace=True)
+observed_P = observed_P.loc[start_date:end_date]
+output= pd.concat([observed_P, df_7_11.iloc[:, 1]])
+print(output)
+observed_Q = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 6, header=0)
+
+
+#print(observed_P)
+
+evap_lesse = observed_P["Evaporation"]
+
+total_discharge = np.zeros(len(observed_P))
+
+precip_total_1 = np.zeros(len(observed_P)+12)
+precip_total_9 = np.zeros(len(observed_P)+12)
 
 #
 # Calculate Unit Hydrographs
 #
-Q_1 = np.zeros([len(precip_lesse), len(precip_lesse)+12])
-Q_9 = np.zeros([len(precip_lesse), len(precip_lesse)+12])
+Q_1 = np.zeros([len(observed_P), len(observed_P)+12])
+Q_9 = np.zeros([len(observed_P), len(observed_P)+12])
 
 #
 # Main
@@ -159,20 +167,20 @@ Q_9 = np.zeros([len(precip_lesse), len(precip_lesse)+12])
  
 UH1 = fun_UH1(x_4)
 UH2 = fun_UH2(x_4)
-for t in range(len(precip_lesse)):
-    P = precip_lesse.iat[t]
+for t in range(len(observed_P)):
+    P = observed_P.iat[t]
     E = evap_lesse.iat[t]
     [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
     total_discharge[t] = Q / 86400 * area * 1000
 
 if plotprocess == True:
-    plt.plot(discharge_lesse[warmup:])
-    plt.plot(total_discharge)
+    plt.plot(observed_P["Date"], total_discharge)
+    plt.plot(observed_Q["Date"], observed_Q["Q"])
     plt.ylabel("Discharge (m^3/s)")
     plt.xlabel("Time (Days)")
-    plt.legend(["Measured", "Simulated"])
-    plt.title("Measured versus simulated discharge")
-    plt.xlim([warmup, len(discharge_lesse)])
+    plt.legend(["Simulated", "Measured"])
+    plt.title("Forecast")
+    #plt.xlim([warmup, len(discharge_lesse)])
     plt.show()
 else: 
     print("done")
