62ff26ab1fce1b3f7bd974da54692b8d7217cbf7
Rick Frazer <pcricky.rf@gmail.com>
2024-01-18 13:36:09 +0100

    Deterministic addition

--- GR4J_ensemble.py
+++ GR4J_ensemble.py
@@ -8,9 +8,9 @@ import datetime
 # Functions
 #
 
-def dataimport(path, interval):                        
+def dataimport(path, interval, skiprows):                        
     # Read out the CSV, define index and datacolumn
-    df = pd.read_csv(path, header=0, index_col=None, skiprows=2, delimiter=",", parse_dates=[0], skipinitialspace=True, dayfirst=True)          
+    df = pd.read_csv(path, header=0, index_col=None, skiprows=skiprows, delimiter=",", parse_dates=[0], skipinitialspace=True, dayfirst=True)          
     # Convert indexcolumn to datetime format                                                                            
     df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y %H:%M',errors='coerce', utc=True, dayfirst=True)      
     # Drop NAN's                                   
@@ -21,12 +21,14 @@ def dataimport(path, interval):
     output = df.groupby(pd.PeriodIndex(df['date'], freq=interval))[df.columns[1:]].sum()
     return output
 
-def ensemble(df, observed_P, R, S, x_1, x_2, x_3, x_4):
+def ensemble(df, deterministic, observed_P, R, S, x_1, x_2, x_3, x_4):
     UH1 = fun_UH1(x_4)
     UH2 = fun_UH2(x_4)
     counter = 0
     for column_name in df.columns:
         P_ensemble = df[column_name]
+        S = 0
+        R = 0
         for t in range(len(observed_P)):
             P = observed_P.iat[t, 0]
             E = observed_P.iat[t, 1]
@@ -40,7 +42,22 @@ def ensemble(df, observed_P, R, S, x_1, x_2, x_3, x_4):
         counter += 1
         output[counter] = total_discharge
         print(counter)
-            
+    
+    S = 0
+    R = 0
+    for t in range(len(observed_P)):
+        P = observed_P.iat[t, 0]
+        E = observed_P.iat[t, 1]
+        [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
+        total_discharge[t] = Q / 86400 * area * 1000
+    for u in range(len(P_ensemble)):
+        P = deterministic.iat[u, 0]
+        E = 3
+        [R, S, Q] = update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2)
+        total_discharge[u+t+1] = Q / 86400 * area * 1000
+    counter += 1
+    deterministic_output[counter] = total_discharge
+    print(counter)
 
 def update_timestep(t, P, E, R, S, x_1, x_2, x_3, UH1, UH2):
     """
@@ -149,14 +166,23 @@ forecast = '2021-07-19'
 
 plotprocess = True
 
-df_7_9  = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021070900.csv"), 'D')
-df_7_10 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071000.csv"), 'D')
-df_7_11 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071100.csv"), 'D')
-df_7_12 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071200.csv"), 'D')
-df_7_13 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071300.csv"), 'D')
-df_7_14 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071400.csv"), 'D')
+df_7_9  = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021070900.csv"), 'D', 2)
+df_7_10 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071000.csv"), 'D', 2)
+df_7_11 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071100.csv"), 'D', 2)
+df_7_12 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071200.csv"), 'D', 2)
+df_7_13 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071300.csv"), 'D', 2)
+df_7_14 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/2021071400.csv"), 'D', 2)
 
-deterministic = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx"), 2, header=0, index_col=None, parse_dates=[0])
+deter_10 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_10.csv"), 'D', 0)
+deter_11 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_11.csv"), 'D', 0)
+deter_12 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_12.csv"), 'D', 0)
+deter_13 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_13.csv"), 'D', 0)
+deter_14 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_14.csv"), 'D', 0)
+deter_15 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_15.csv"), 'D', 0)
+deter_17 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_17.csv"), 'D', 0)
+deter_18 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_18.csv"), 'D', 0)
+deter_19 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_19.csv"), 'D', 0)
+deter_21 = dataimport(os.path.join(os.path.dirname(__file__), "forecasts/deterministic_21.csv"), 'D', 0)
 
 observed_P = pd.read_excel(os.path.join(os.path.dirname(__file__), "forecasts/as5.xlsx", ), 4, header=0, index_col=None)
 observed_P['date'] = pd.to_datetime(observed_P['date'], format='%Y-%m-%d', errors='coerce', utc=True) 
@@ -172,18 +198,18 @@ precip_total_9 = np.zeros(len(observed_P)+12)
 
 Q_1 = np.zeros([len(observed_P), len(observed_P)+12])
 Q_9 = np.zeros([len(observed_P), len(observed_P)+12])
-ensemble(df_7_10, "2021-07-10")
 
 observed_P = observed_P.loc[start_date:end_date]
 total_discharge = np.zeros(len(observed_P)+10,)
 output = pd.DataFrame(index=range(len(observed_P)+len(df_7_9)))
+deterministic_output = pd.DataFrame(index=range(len(observed_P)+len(df_7_9)))
 observed_Q = observed_Q.loc[:end_date]
 
 #
 # Main
 #
 
-ensemble(df_7_9, observed_P, R, S, x_1, x_2, x_3, x_4)
+ensemble(df_7_9, deter_10, observed_P, R, S, x_1, x_2, x_3, x_4)
 
 # UH1 = fun_UH1(x_4)
 # UH2 = fun_UH2(x_4)
@@ -195,6 +221,7 @@ ensemble(df_7_9, observed_P, R, S, x_1, x_2, x_3, x_4)
 
 if plotprocess == True:
     plt.plot(pd.date_range(start=start_date, end=forecast, freq='D'), output, linewidth=0.2, color='b', alpha=0.5)
+    plt.plot(pd.date_range(start=start_date, end=forecast, freq='D'), deterministic_output, linewidth=0.5, color='r', alpha=1)
     plt.plot(observed_Q.index, observed_Q["Q"], 'g')
     plt.vlines(datetime.date(2021, 7, 9), 0, 60, 'r', ':')
     plt.ylabel("Discharge (m^3/s)")
